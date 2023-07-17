2ac6aed2d34453399ffec611d0740227e780e123
Pham Thong <phamthong2532003@gmail.com>
2023-07-17 12:11:02 +0700

    Cập nhật source code mới

--- nodejs/src/routes/me.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const meController = require('../app/controllers/MeController')
-
-
-router.use('/stored/courses',meController.storedCourses);
-router.use('/trash/courses',meController.trashCourses);
-
-module.exports = router;
--- vite-project/src/todo-list/constant.jsx
+++
@@ -1,3 +0,0 @@
-export const set_job = 'set_job'
-export const add_job = 'add_job'
-export const delete_job = 'delete_job'
