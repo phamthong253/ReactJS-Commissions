3f5a96a6d6b217b02e0bafe6483eea36833a0f7d
phamthong253 <108605402+phamthong253@users.noreply.github.com>
2023-09-10 15:45:19 +0700

    Update collection.component.jsx

--- src/component/home/collection.component.jsx
+++ src/component/home/collection.component.jsx
@@ -113,6 +113,7 @@ function Collection() {
                className="flex w-1/3 flex-wrap">
                 <div className="w-full p-1 md:p-2">
                   <img
+                    onClick={() => handleImageClick(item.imgSrc)}
                     alt="gallery"
                     className="block h-full w-full rounded-lg object-cover object-center"
                     src={item.imgSrc}
